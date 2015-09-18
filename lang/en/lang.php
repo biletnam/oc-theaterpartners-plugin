852bec98b6853038d2bf38a95a80ed27495d4060
Imyarek Bezymyannyi <github@abnmt.com>
2015-09-19 01:17:02 +0300

    Lang

--- lang/en/lang.php
+++ lang/en/lang.php
@@ -18,6 +18,14 @@ return [
         'preview_title' => 'Preview Partner',
         'list_title' => 'Manage Partners',
         'new' => 'New Partner',
+        'title' => 'abnmt.theaterpartners::lang.partner.title',
+        'title_placeholder' => 'abnmt.theaterpartners::lang.partner.title_placeholder',
+        'slug' => 'abnmt.theaterpartners::lang.partner.slug',
+        'slug_placeholder' => 'abnmt.theaterpartners::lang.partner.slug_placeholder',
+        'tab_manage' => 'abnmt.theaterpartners::lang.partner.tab_manage',
+        'cover' => 'abnmt.theaterpartners::lang.partner.cover',
+        'logo_color' => 'abnmt.theaterpartners::lang.partner.logo_color',
+        'logo_bw' => 'abnmt.theaterpartners::lang.partner.logo_bw',
     ],
     'partners' => [
         'menu_label' => 'Partners',
@@ -34,6 +42,11 @@ return [
         'preview_title' => 'Preview Category',
         'list_title' => 'Manage Categories',
         'new' => 'New Category',
+        'name' => 'abnmt.theaterpartners::lang.category.name',
+        'partners' => 'abnmt.theaterpartners::lang.category.partners',
+        'name_placeholder' => 'abnmt.theaterpartners::lang.category.name_placeholder',
+        'slug' => 'abnmt.theaterpartners::lang.category.slug',
+        'slug_placeholder' => 'abnmt.theaterpartners::lang.category.slug_placeholder',
     ],
     'categories' => [
         'menu_label' => 'Categories',
@@ -42,5 +55,10 @@ return [
         'delete_selected_confirm' => 'Delete the selected categories?',
         'delete_selected_success' => 'Successfully deleted the selected categories.',
         'delete_selected_empty' => 'There are no selected :name to delete.',
+    ],
+    'post' => [
+        'title' => 'abnmt.theaterpartners::lang.post.title',
+        'category' => 'abnmt.theaterpartners::lang.post.category',
+        'published' => 'abnmt.theaterpartners::lang.post.published',
     ],
 ];
